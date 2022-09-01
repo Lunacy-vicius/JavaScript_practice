c5aacb5ea7652af4e02f77d02afbf1dd05de980a
Ricardo Cisneros <59590506+Lunacy-vicius@users.noreply.github.com>
2022-08-31 21:47:28 -0500

    otro commit

--- CursoJSModerno/08-Arrays/js/04-app.js
+++ CursoJSModerno/08-Arrays/js/04-app.js
@@ -3,4 +3,4 @@ const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"];
 meses[0] = "Nuevo mes";
 meses[7] = "Ultimo mes";
 //si pusieramos una posicion "11" JavaScript lo interpretaria y no crearia mas elementos como lo hacen otros lenguajes simplemente se salta a esa posicion.
-console.table(meses)
+console.table(meses);
